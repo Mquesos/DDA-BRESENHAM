9cf8b97a581c3153fc72ec3f9d60d63ac2e9e398
Mquesos <molinavillanuevak@gmail.com>
2019-02-19 02:44:16 -0600

    DDA Completo Version 1 Validaciones

--- ALGORITMOS/Form1.cs
+++ ALGORITMOS/Form1.cs
@@ -12,6 +12,7 @@ namespace ALGORITMOS
 {
     public partial class Form1 : Form
     {
+        Pen pen = new Pen(Color.Green, 3);
         List<ClaseCoordenada> listaCoordenadas;//Clase que se comporta como punto Point
         public Form1()
         {
@@ -26,7 +27,7 @@ namespace ALGORITMOS
         }
         private void dibujarCoordenada(ClaseCoordenada coordenada)
         {
-            Pen pen = new Pen(Color.Green, 3);
+
             float x, y;
             x = coordenada.getX() ;
             y = coordenada.getY() ;
@@ -55,6 +56,10 @@ namespace ALGORITMOS
             xf = listaCoordenadas[1].getX();
             yf = listaCoordenadas[1].getY();
 
+            Console.WriteLine("------------- captura de coordenadas ------------------");
+            Console.WriteLine("punto x inicial: (" + xi + ", " + yi + ")");
+            Console.WriteLine("punto y final: (" + xf + ", " + yf + ")");
+
             float dx;
             float dy;       
             float m;
@@ -62,65 +67,126 @@ namespace ALGORITMOS
             int kx = 0;
             int ky = 0;
 
+            float d1;
+            float d2;
+            float d3;
+        
+
             dx = xf - xi;
             dy = yf - yi;
 
             m = (dy / dx);
             b = (-xi * m) + yi;
 
-            if(xi < xf | yf < xi )
-            {
-                if (xi < xf)
-                {
-                    int aux = xi;
-                    xi = xf;
-                    xf = aux;
-                }
-                else
-                {
-                    int auxy = yi;
-                    yi = yf;
-                    yf = auxy;
-                }
-            }
-
+            //Verificar si la recta es vertical
             if (dx == 0)
             {
                 if (dy > 0)
                 {
                     for (int k = yi; k <= yf; k++)
                     {
-                        Pen pen = new Pen(Color.Green, 3);
                         panel1.CreateGraphics().DrawEllipse(pen, xi, k, 5, 5);
                     }
                 }
             }
-            else if (dy == 0)
+            //Verificar si la recta es horizontal
+            if (dy == 0)
             {
                 if (dx > 0)
                 {
                     for (int k = xi; k <= xf; k++)
                     {
-                        Pen pen = new Pen(Color.Green, 3);
                         panel1.CreateGraphics().DrawEllipse(pen, xi, k, 5, 5);
                     }
                 }
             }
-            else
+            //verificar el sentido de la recta
+            if(dx < 0 & dy < 0 | dx > 0 & dy < 0) 
             {
+                float inx = xi;
+                float iny = yi;
+
+                xi = xf;
+                xf = (int)inx;//casteo a int
+
+                yi = yf;
+                yf = (int)iny;//casteo a int
+
+                dx = xf - xi;
+                dx = yf - yi;
+            }
+
+            //Verificar si X es el eje independiente de la recta
+            if(Math.Abs(dx) > Math.Abs(dy))
+            {
+                m = dy / dx;
+                //Verificar si la pendiente es mayor o menor que cero
+                if(m > 0)
+                {
+                    for (kx = 1; kx < dx; kx++)
+                    {
+                        d1 = m * kx - ky;
+                        d2 = (ky + 1) - m * kx;
+                        d3 = d1 - d2;
+
+                        if (d3 > 0)
+                            ky++;
+                        panel1.CreateGraphics().DrawEllipse(pen, xi + kx, yi + ky, 5, 5);
+                    }
+                }
+                //Verificar si la pendiente es menor que cero
+                else
+                {
+                    for(kx = -1; kx > dx; kx--)
+                    {
+                        d1 = m * kx - ky;
+                        d2 = (ky + 1) - m * kx;
+                        d3 = d1 - d2;
+
+                        if (d3 > 0)
+                            ky++;
+                        panel1.CreateGraphics().DrawEllipse(pen, xi + kx, yi + ky, 5, 5);
+                    }
+                }
+            }
+            else if(Math.Abs(dy) > Math.Abs(dx))
+            {
+                m = dx / dy;
+
+                //Verifica si la dependiente es mayor que cero
+                if(m > 0)
+                {
+                    for (ky = 1; ky < dy; ky++)
+                    {
+                        d1 = m * ky - kx;
+                        d2 = (kx + 1) - m * ky;
+                        d3 = d1 - d2;
+
+                        if (d3 > 0)
+                            kx++;
+                        panel1.CreateGraphics().DrawEllipse(pen, xi + kx, yi + ky, 5, 5);
+                    }
+                }
+                //Verifica si la pendiente es menor que cero
+                if(m < 0)
+                {
+                    m = Math.Abs(m);
+
+                    for(ky = 1; ky < dy; ky++)
+                    {
+                        d1 = m * ky - kx;
+                        d2 = (kx + 1) - m * ky;
+                        d3 = d1 - d2;
 
 
+                        if (d3 > 0)
+                            kx++;
 
+                        panel1.CreateGraphics().DrawEllipse(pen, xi - kx, yi + ky, 5, 5);
 
-
-
+                    }
+                }
             }
         }
-        /*for (int i = 0; i < listaCoordenadas[1].getX(); i++)
-        {
-            ClaseCoordenada CoordenadasDDA = new ClaseCoordenada();
-
-
-        }*/
     }
 }
